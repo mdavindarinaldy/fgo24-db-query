11e351aa3942f4f213d8ff37292da5e1b330579c
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-06-17 10:05:13 +0700

    feat: add new filtering query

--- challenge1.sql
+++ challenge1.sql
@@ -2,9 +2,14 @@ SELECT * FROM movies WHERE year > 2000
 -- ORDER BY year DESC
 ;
 
-SELECT * FROM actors 
-WHERE 
+SELECT * FROM actors WHERE 
 -- (first_name ILIKE '%s') 
 -- OR 
 (last_name ILIKE '%s')
+;
+
+SELECT * FROM movies WHERE
+(rankscore BETWEEN 5 AND 7)
+AND (year BETWEEN 2004 AND 2006)
+ORDER BY rankscore DESC
 ;
